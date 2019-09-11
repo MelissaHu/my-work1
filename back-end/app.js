d2b8a24a3a032772abdf14f450d11eeaebba5221
melissaHu <hlp.pingping@gmail.com>
2019-09-11 09:54:11 +0800

    解决跨域

--- back-end/app.js
+++ back-end/app.js
@@ -5,9 +5,24 @@ const fs = require('fs')
 const app = new Koa()
 const router = new Router()
 
+app.use(cors({
+    origin: function(ctx) {
+        if (ctx.url === '/test') {
+            return "*"; // 允许来自所有域名请求
+        }
+        return 'http://localhost:9000'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+
+
 router.get('/getSchemeList', async ctx => {
-  await cors()
-  ctx.body = JSON.parse(fs.readFileSync('./static/schemeItem.json'))
+    //await cors()
+    ctx.body = JSON.parse(fs.readFileSync('./static/schemeItem.json'))
 })
 
 app.use(router.routes()).use(router.allowedMethods())
--- font-end/src/main.js
+++ font-end/src/main.js
@@ -5,12 +5,13 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
+    // Vue.prototype.$axios = axios
+Vue.prototype.$ajax = axios
 Vue.use(axios)
-/* eslint-disable no-new */
+    /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
